db24b86d68246cdb8331c2bb2da38905f786cc3e
jacquelinebarajas-eng <jacqueline.barajas@cimat.mx>
2026-09-21 01:22:31 +0000

    Ingresar problema 1

--- solucionador.py
+++ solucionador.py
@@ -1,5 +1,6 @@
 from amplpy import AMPL
 
+# -------
 # 1. Inicializa AMPL
 ampl = AMPL()
 
@@ -13,9 +14,12 @@ ampl.set_option("solver", "highs")
 ampl.solve()
 
 # 5. Muestra los resultados en pantalla
+#'''
+#        PROBLEMA 3
 print("\n--- RESULTADOS ---")
 print("Ganancia Total Máxima (Z):", ampl.get_objective("z").value())
 print("Unidades de x1:", ampl.get_variable("x1").value())
 print("Unidades de x2:", ampl.get_variable("x2").value())
 print("Unidades de x3:", ampl.get_variable("x3").value())
 print("Unidades de x4:", ampl.get_variable("x4").value())
+#'''
